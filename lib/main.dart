0e8d5e5c027b2fb265cd7de2a88e9e73fa8bb196
luisgustavoo <luisgustavovierasantos@gmail.com>
2022-12-10 03:24:13 -0300

    Adicionando firebase massage ao projeto

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,46 @@
+import 'dart:developer';
+
 import 'package:device_preview/device_preview.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:google_api_availability/google_api_availability.dart';
 import 'package:job_timer/app/job_timer_router.dart';
 import 'package:job_timer/firebase_options.dart';
+
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  // await Firebase.initializeApp();
+
+  log('Handling a background message: ${message.messageId}');
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+  log(fcmToken ?? '');
+  final availability = await GoogleApiAvailability.instance
+      .checkGooglePlayServicesAvailability(true);
+  log('$availability');
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  FirebaseMessaging.onMessage.listen((message) {
+    log('Got a message whilst in the foreground!');
+    log('Message data: ${message.data}');
+
+    if (message.notification != null) {
+      log('Message also contained a notification: ${message.notification}');
+    }
+  });
 
   runApp(
     DevicePreview(
       builder: (context) => JobTimerRouter(),
       // ignore: avoid_redundant_argument_values
-      enabled: !kReleaseMode,
+      enabled: false,
     ),
   );
 }
